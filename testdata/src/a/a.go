43dec28b282ee76a18c9b01f248948742b49a295
masimacbook <masi19bw@gmail.com>
2020-09-01 10:46:17 +0900

    add Example to README and test file

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,42 @@
-package a
+package main
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+
+type Person struct {
+	Name string
+	JobID int
 }
 
+type Job struct {
+	JobID int
+	Name string
+}
+
+func main(){
+
+	cnn, _ := sql.Open("mysql", "user:password@tcp(host:port)/dbname")
+
+	rows, _ := cnn.Query("SELECT name, job_id FROM person")
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var person Person
+		if err := rows.Scan(&person.Name,&person.JobID); err != nil {
+			log.Fatal(err)
+		}
+
+		var job Job
+		// This is N+1 Query
+		if err := cnn.QueryRow("SELECT job_id, name FROM Job WHERE job_id = ?",person.JobID).Scan(&job.JobID,&job.Name); err != nil { //want "N+1 query"
+			log.Fatal(err)
+		}
+		fmt.Println(person.Name,job.Name)
+	}
+
+}
