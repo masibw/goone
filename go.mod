d945ac0007f5dd49cefbd487b30a35b81e9b3228
masimacbook <masi19bw@gmail.com>
2020-09-04 16:44:56 +0900

    gorp,sqlx対応

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/masibw/go_one
 go 1.14
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gostaticanalysis/analysisutil v0.1.0
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/lib/pq v1.8.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/tools v0.0.0-20200831203904-5a2aa26beb65
+	gopkg.in/gorp.v1 v1.7.2 // indirect
 )
---
+++ testdata/src/sqlx/sqlx.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+type Person struct {
+	Name  string`db:"name"`
+	JobID int`db:"job_id"`
+}
+
+type Job struct {
+	JobID int`db:"job_id"`
+	Name  string`db:"name"`
+}
+
+func forStmt() {
+
+	cnn, _ := sqlx.Connect("mysql", "user:password@tcp(host:port)/dbname")
+
+	rows, _ := cnn.Queryx("SELECT name, job_id FROM persons")
+
+	for rows.Next() {
+		var person Person
+		if err := rows.StructScan(&person); err != nil {
+			log.Fatal(err)
+		}
+
+		var job Job
+		// This is N+1 Query
+		err := cnn.Get(&job,"SELECT job_id, name FROM Jobs WHERE job_id = ?", person.JobID)//want "this query called in loop"
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(person.Name, job.Name)
+	}
+
+}
+
+func main() {
+	cnn, _ := sqlx.Connect("mysql", "user:password@tcp(host:port)/dbname")
+
+	rows, _ := cnn.Queryx("SELECT name, job_id FROM persons")
+
+	var persons []Person
+	for rows.Next() {
+		var person Person
+		if err := rows.StructScan(&person); err != nil {
+			log.Fatal(err)
+		}
+		persons = append(persons,person)
+	}
+	for _,person := range persons{
+		var job Job
+		// This is N+1 Query
+		err := cnn.Get(&job,"SELECT job_id, name FROM Jobs WHERE job_id = ?", person.JobID)//want "this query called in loop"
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(person.Name, job.Name)
+	}
+}
